ac9ee66e52b083667f2e1b6c2a82eb3c7aa4cf24
yulia <nappestok@gmail.com>
2022-03-25 10:36:46 +0300

    completed and all todos views addded

--- todos_app/urls.py
+++ todos_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'todos'
 urlpatterns = [
-    path('', views.CurrentTodosView.as_view(), name="main"),
+    path('', views.CurrentTodosView.as_view(), name="currenttodos"),
+    path('all/', views.AllTodosView.as_view(), name="alltodos"),
+    path('completed/', views.CompletedTodosView.as_view(), name="completedtodos"),
 ]
--- todos_app/views.py
+++ todos_app/views.py
@@ -6,7 +6,7 @@ from .models import Todos
 
 
 class CurrentTodosView(LoginRequiredMixin, generic.ListView):
-    template_name = 'todos_app/main.html'
+    template_name = 'todos_app/todos.html'
     context_object_name = 'todos'
 
     def get_queryset(self):
@@ -19,4 +19,40 @@ class CurrentTodosView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         data = super().get_context_data(**kwargs)
         data["title"] = "Current todos"
+        data["todos_status"] = "current"
+        return data
+
+
+class AllTodosView(LoginRequiredMixin, generic.ListView):
+    template_name = 'todos_app/todos.html'
+    context_object_name = 'todos'
+
+    def get_queryset(self):
+        todos = Todos.objects.filter(
+            user=self.request.user
+        ).values('id', 'title', 'description', 'priority')
+        return todos
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data["title"] = "All todos"
+        data["todos_status"] = ""
+        return data
+
+
+class CompletedTodosView(LoginRequiredMixin, generic.ListView):
+    template_name = 'todos_app/todos.html'
+    context_object_name = 'todos'
+
+    def get_queryset(self):
+        todos = Todos.objects.filter(
+            user=self.request.user,
+            completed_at__isnull=False
+        ).values('id', 'title', 'description', 'priority')
+        return todos
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data["title"] = "Completed todos"
+        data["todos_status"] = "completed"
         return data

ef278590e7d74bd9d0b69a6df84cbc8cad428fe0
yulia <nappestok@gmail.com>
2022-03-29 22:21:17 +0300

    update user api added

--- accounts_app/api/views/accounts.py
+++ accounts_app/api/views/accounts.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.models import User
+from rest_framework import permissions
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 
 from ...api.serializers.accounts import UserSerializer
 
 
-class UserViewSet(GenericViewSet, CreateModelMixin):
+class IsCreationOrIsAuthenticated(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'create' or request.user.is_authenticated:
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class UserViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-
+    permission_classes = (IsCreationOrIsAuthenticated, )
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -60,6 +59,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'todo_list.urls'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -91,7 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'todo_list.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -105,7 +106,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,7 +125,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -138,7 +137,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

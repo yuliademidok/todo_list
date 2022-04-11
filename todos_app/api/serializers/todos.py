86b983e7909dbf6551e459a0ab2f03e310a1560a
yulia <nappestok@gmail.com>
2022-04-11 17:31:33 +0200

    subtasks retrieving in todo serializer updated

--- todos_app/api/serializers/todos.py
+++ todos_app/api/serializers/todos.py
@@ -3,37 +3,6 @@ from rest_framework import serializers
 from todos_app.models import Todos
 
 
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todos
-        exclude = ('user', 'parent_id', )
-        read_only_fields = ('completed_at', )
-
-    publisher_user = serializers.HiddenField(
-        default=serializers.CurrentUserDefault(),
-        source='user'
-    )
-
-    subtasks = serializers.SerializerMethodField()
-
-    def get_subtasks(self, instance) -> list:
-        return Todos.objects.filter(parent_id=instance.id).values()
-
-
-class CompleteTodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todos
-        read_only_fields = (
-            'title',
-            'description',
-            'created_at',
-            'completed_at',
-            'priority',
-            'parent_id'
-        )
-        exclude = ('user', )
-
-
 class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todos
@@ -54,3 +23,37 @@ class SubtaskSerializer(serializers.ModelSerializer):
                 data['parent_id'] not in Todos.objects.filter(user=user_id, parent_id__isnull=True):
             raise serializers.ValidationError('Invalid parent id')
         return data
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todos
+        exclude = ('user', 'parent_id', )
+        read_only_fields = ('completed_at', )
+
+    publisher_user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        source='user'
+    )
+
+    # subtasks = serializers.SerializerMethodField()
+    subtasks = SubtaskSerializer(many=True, read_only=True)
+
+    # def get_subtasks(self, instance) -> list:
+    #     return Todos.objects.filter(parent_id=instance.id).values()
+
+
+class CompleteTodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todos
+        read_only_fields = (
+            'title',
+            'description',
+            'created_at',
+            'completed_at',
+            'priority',
+            'parent_id'
+        )
+        exclude = ('user', )
+
+
